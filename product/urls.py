5b64c847b95f12f11dfa7903d75828313d4f5316
Damu2509 <reddivaridamu25091999@gmail.com>
2021-04-11 11:02:44 +0530

    To be continued with the forms that are built in django

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path,include 
-from .views import product_detail_view
+from .views import product_detail_view, product_create_view
 
 urlpatterns = [ 
     path('detail/', product_detail_view, name = 'detail'),
-    
+    path('create/', product_create_view, name = 'create'),
 
 ]
--- product/views.py
+++ product/views.py
@@ -7,6 +7,21 @@ from product.models import Products
 from .models import Products
 from django.forms import forms
 
+from .forms import ProductForm
+
+
+def product_create_view(request):
+
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+    
+    context = {
+        'form': form
+    }
+
+    return render(request, 'product/product_create.html', context)
 
 def product_detail_view(request):
     
