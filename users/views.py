6bc104459a77e2b53679a7eae9e4423cc847587e
Damu2509 <reddivaridamu25091999@gmail.com>
2021-06-04 10:42:46 +0530

    added styles

--- users/views.py
+++ users/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from product.forms import RegisterForm
 # Create your views here.
 
-
+from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
 
-        form = RegisterForm(request.POST or None)
+        form = UserCreationForm()
 
         if form.is_valid():
             form.save()
