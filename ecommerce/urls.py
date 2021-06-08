019877db779affcd277bc9e0c96ef419bd5ebed3
Damu2509 <reddivaridamu25091999@gmail.com>
2021-06-08 10:44:56 +0530

    Messed up with these stuff

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('product.urls'), name = 'product'),
     path('users/', include('users.urls'), name = 'users'),
 
 
