b2f486e66bb5535c000ee37743906b43792236b2
Damu2509 <reddivaridamu25091999@gmail.com>
2021-06-10 10:29:14 +0530

    going to work hard with these things

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 
+from .views import userRegistration
+
 app_name = 'users'
 
 urlpatterns = [
 
-
+path('register/', userRegistration, name = 'registration')
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,1 +1,8 @@
 from django.shortcuts import render
+from .forms import UserRegistration
+
+def userRegistration(request):
+
+    form = UserRegistration(request.POST)
+
+    return render(request, 'users/userRegistration.html', {'form' : form})
