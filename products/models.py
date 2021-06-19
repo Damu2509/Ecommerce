f1d2d5b19fc9dde5972b8710a1dbeec175fb9c65
Damu2509 <reddivaridamu25091999@gmail.com>
2021-06-19 21:17:02 +0530

    Separated categories with items

--- products/models.py
+++ products/models.py
@@ -2,11 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+class itemCategories(models.Model):
+
+    category  = models.CharField( max_length = 100, null = True)
+    title = models.CharField( max_length = 250, null = True )
+    image_url  = models.CharField(max_length = 250, null= True)
+  
 class Items(models.Model):
 
-    category  = models.CharField( max_length = 100)
-    title = models.CharField( max_length = 250 )
-    image_url  = models.CharField(max_length = 250, null= True)
-    tag = models.CharField( max_length = 100 )
+    title = models.CharField(max_length  = 250, null = True)
+    image_url = models.CharField(max_length =  250 , null = True)
+    tag = models.CharField( max_length = 100, null = True )
     description = models.TextField()
-    price  = models.IntegerField( default = 0 )
+    price  = models.IntegerField( default = 0, null = True )
+
+
